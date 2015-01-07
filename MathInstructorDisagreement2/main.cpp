0448faa635a40512506427131b7a58ff06b55ba9
mt2522577 <melvintran@icloud.com>
2015-01-07 11:49:17 -0800

    Signed-off-by: mt2522577 <melvintran@icloud.com>

---
+++ MathInstructorDisagreement2/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Melvin Tran
+ * Created on January 7, 2015, 11:18 AM
+ * Purpose: How easy computer can make mistake 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User libraries
+
+//Global Constants
+
+//Functions Prototype
+
+//Execution begin here!
+int main(int argc, char** argv) {
+    //Declare 3 variables
+    float a,b,c;
+    //Prompt for a and b
+    cout<<"Input 2 float vales"<<endl;
+    cin>>a>>b;
+    //Sum the values
+    c=a+b;
+    //Output the results
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    //exit stage right!
+    return 0;
+}
+

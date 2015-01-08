beabf28f08134df19a22d72b882c78a37013e413
mt2522577 <melvintran@icloud.com>
2015-01-08 09:14:56 -0800

    Signed-off-by: mt2522577 <melvintran@icloud.com>

---
+++ Hmwk/Gaddis_8thEd_Ch2_Q16/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Melvin Tran
+ * Created on January 8, 2015, 9:02 AM
+ * Purpose: Diamond Pattern
+ */
+
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+cout<<"    *     "<<endl;
+cout<<"   ***     "<<endl;
+cout<<"  *****     "<<endl;
+cout<<" *******     "<<endl;
+cout<<"  *****     "<<endl;
+cout<<"   ***     "<<endl;
+cout<<"    *     "<<endl;
+    return 0;
+}
+
---
+++ Hmwk/Gaddis_8thEd_Ch2_Q18/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Melvin Tran
+ * Created on January 8, 2015, 7:50 AM
+ * Purpose: Energy Drink Consumption
+ */
+
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+const char CONVPCT=100;//Percent conversion
+//Function Prototype
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+//Declare variables
+short custSrv=12467; //Customer Surveyed
+char perEDrk=14;//percent Energy Drinkers
+char perCDrk=64;//percent Citrus Drinkers
+//Calculate customer numbers requested
+short custE=custSrv*perEDrk/CONVPCT;//Energy Drinkers
+short custC=custE*perCDrk/CONVPCT;//Citrus Drinkers
+//Output the results
+cout<<"Customers surveyed that are Energy Drinkers = ";
+cout<<custE<<endl;
+cout<<"Customers surveyed that are Citrus Drinkers = ";
+cout<<custC<<endl;
+
+//Exit stage right
+return 0;
+}

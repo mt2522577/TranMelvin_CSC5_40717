46ce102fb1acb3209d449e611e7a8faed3f85727
Melvin Tran <Melvintran@gmail.com>
2015-02-07 00:28:12 -0800

    add more functions

--- Project2/Project1_V2/main.cpp
+++ Project2/Project1_V2/main.cpp
@@ -18,7 +18,9 @@ using namespace std;
 //Global Function
 
 //Function Prototype
-int card1,card2,card3,card4;//Didnt work when it was in the main
+//Didn't work when it was in the main
+int card1,card2,card3,card4,cardtot,dcrd1,dcrd2,dcrd3,dcrd4;
+char choice;
 void clearScreen() // used to clear the screen after the game
 {
     for(int i =0; i <50; i++)
@@ -28,35 +30,107 @@ void clearScreen() // used to clear the screen after the game
 }
 //Execution Begins Here!
 int main(int argc, char** argv) {
-    void dealCards();//Didn't work when I put it above the main
-    //Explain the game to the user
-    cout<<"Welcome to the card game 21."<<endl
-        <<"In order to win you'll have to beat the dealer's hand"<<endl
-        <<"But if you get 21 you automatically win!"<<endl
-        <<"Or when the dealer bust!!!"<<endl
-        <<"Please press enter when ready!"<<endl;
+    void dCards();//Didn't work when I put it above the main
+    void delCrd3();
+    void delCrd4();
+    void dealrT();
+        for(int a=1;a<=27;a++){//creating a border for the upper text
+            cout<<"X*";}//Using 2 dimension arrays
+            cout<<endl;
+            cout<<endl;
+            //Explain the game to the user
+            cout<<"Welcome to the card game 21."<<endl
+                <<"In order to win you'll have to beat the dealer's hand"<<endl
+                <<"But if you get 21 you automatically win!"<<endl
+                <<"Or when the dealer bust!!!"<<endl
+                <<"Please press enter when ready!"<<endl;
+            cout<<endl;
+            cout<<endl;    
+        //Lower border of the text
+        for(int b=1;b<=27;b++){
+            cout<<"X*";}
     cin.get();//Use so user can press enter
     clearScreen();
+    for(int a=1;a<=14;a++){//creating a border for the upper text
+        cout<<"21*";}//Using 2 dimension arrays
+        cout<<endl;
+        cout<<endl;
     cout<<"Lets play a game!"<<endl
         <<"You get two cards and if you want to hit"<<endl
         <<"please press the 'h' key"<<endl
         <<"if you rather stay and take a chance"<<endl
         <<"please press the 's' key."<<endl
         <<"Press enter when ready!"<<endl;
+    cout<<endl;
+    //Lower border of the text
+    for(int b=1;b<=14;b++){
+        cout<<"21*";}
     cin.get();
-    dealCards();
+    cout<<endl;
+    cout<<endl;
+    dCards();
     cout<<"Card 1: "<<card1<<endl
         <<"Card 2: "<<card2<<endl;
+    cardtot=card1+card2;
+    cout<<"Your card total is "<<cardtot<<endl;
+    cout<<"Do you want to hit or stay?"<<endl;
+    cin>>choice;
+    cout<<endl;
+    if (choice=='h'||choice=='H'){
+                delCrd3();
+                 cout<<"Card 1 : "<<card1<<endl;
+    cout<<"Card 2 : "<<card2<<endl;
+    cout<<"Card 3 : "<<card3<<endl;
+    cardtot=cardtot+card3;
+    cout<<"Card total : "<<cardtot<<endl;
+    }
+    cout<<"Do you want to hit or stay?"<<endl;
+    cin>>choice;
+    cout<<endl;
+    if(choice=='s'||choice=='S'){
+        dealrT();
+    }
+    if (choice=='h'||choice=='H'){
+                delCrd4();
+    cout<<"Card 1 : "<<card1<<endl;
+    cout<<"Card 2 : "<<card2<<endl;
+    cout<<"Card 3 : "<<card3<<endl;
+    cout<<"Card 4 : "<<card4<<endl;
+    cardtot=cardtot+card4;
+    cout<<"Card total : "<<cardtot<<endl;
+    }
+    if(choice=='s'||choice=='S'){
+                dealrT();
+    }
     
-        //Declare Variables
-        //const int SIZE=52;//Constant for the array size
-       // int deck[SIZE];//An array for 52 cards
-          
+            if(cardtot>21){
+                cout<<"You Bust!!! Try your luck next time!";
+            }
+            if(cardtot==21){
+                cout<<"You Win!!!!"<<endl;
+            }
     return 0;
 }
-void dealCards(){
+void dCards(){
     srand(time(NULL));
     card1=rand()%11+1;
     card2=rand()%11+1;
     return;
                  }
+void delCrd3(){
+    srand(time(NULL));
+    card3=rand()%11+1;
+    return;
+}
+ void delCrd4(){
+    srand(time(NULL));
+    card4=rand()%11+1;
+ }
+void dealrT(){
+    srand(time(NULL));
+    dcrd1=rand()%11+1;
+    dcrd2=rand()%11+1;
+    dcrd3=rand()%11+1;
+    dcrd4=rand()%11+1;
+    return;
+}
